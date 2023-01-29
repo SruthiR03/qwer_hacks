ccf1c5c341e6ea27dbd9f25a3e65e015b69d9cdb
anaghasrivatsav <anagha.v.srivatsav@gmail.com>
2023-01-28 17:58:49 -0800

    updates for hospital cards

--- mern_a_to_z_client/src/components/BookCard.js
+++ mern_a_to_z_client/src/components/BookCard.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import styles from '../App.css';
+
 
 const BookCard = (props) => {
   const book = props.book;
 
   return (
     <div className='card-container'>
-      <img
-        src='https://images.unsplash.com/photo-1495446815901-a7297e633e8d'
-        alt='Books'
-        height={200}
-      />
+     
       <div className='desc'>
+       
         <h2>
-          <Link to={`/show-book/${book._id}`}>{book.name}</Link>
+          <strong>
+          <Link to={`/show-book/${book._id}`}> {  book.name}</Link>
+          </strong>
         </h2>
+    
         <h3>{book.address}</h3>
         <p>{book.zipcode}</p>
       </div>
