45f5bd78c0b59ae3375f93038a615925cccfa3b9
Arathi Nair <arathinair@g.ucla.edu>
2023-01-29 03:15:51 -0800

    doctor forms

--- mern_a_to_z_client/src/components/CreateDoctor.js
+++ mern_a_to_z_client/src/components/CreateDoctor.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Graph from "./BarGraph";
+import DoctorForm from "./ReviewDoctorForm";
+import Radio from "./RadioButtons";
 
 const CreateDoctor = (props) => {
   // Define the state with useState hook
   const navigate = useNavigate();
   const [doctor, setDoctor] = useState({
-    name: '',
-    medical_expertise: '',
-    racial_inclusivity: '',
-    timeliness: '',
-    pronouns: '',
-    consideration: '',
+    name: "",
+    medical_expertise: "",
+    racial_inclusivity: "",
+    timeliness: "",
+    pronouns: "",
+    consideration: "",
   });
 
   const onChange = (e) => {
@@ -24,109 +28,79 @@ const CreateDoctor = (props) => {
     e.preventDefault();
 
     axios
-      .post('http://localhost:8082/api/doctors', doctor)
+      .post("http://localhost:8082/api/doctors", doctor)
       .then((res) => {
         setDoctor({
-            name: '',
-            medical_expertise: '',
-            racial_inclusivity: '',
-            timeliness: '',
-            pronouns: '',
-            consideration: '',
+          name: "",
+          medical_expertise: "",
+          racial_inclusivity: "",
+          timeliness: "",
+          pronouns: "",
+          consideration: "",
         });
 
         // Push to /
-        navigate('/doctor-list');
+        navigate(`/show-doctor/${doctor._id}`);
       })
       .catch((err) => {
-        console.log('Error in CreateDoctor!');
+        console.log("Error in CreateDoctor!");
       });
   };
 
   return (
-    <div className='CreateDoctor'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-8 m-auto'>
+    <div className="CreateDoctor">
+      <NavBar />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
             <br />
-            <Link to='/doctor-list' className='btn btn-outline-warning float-left'>
+            <br />
+            <br />
+            <br />
+            <Link
+              to="/doctor-list"
+              className="btn btn-outline-warning float-left"
+            >
               Show Doctor List
             </Link>
           </div>
-          <div className='col-md-8 m-auto'>
-            <h1 className='display-4 text-center'>Add Doctor</h1>
-            <p className='lead text-center'>Create new doctor</p>
+          <div className="col-md-8 m-auto">
+            <h1>
+              {" "}
+              <strong>Add Doctor</strong>{" "}
+            </h1>
+            {/* <p className="lead text-center">Create new doctor</p> */}
 
             <form noValidate onSubmit={onSubmit}>
-              <div className='form-group'>
-                <input
-                  type='text'
-                  placeholder='Name of the Doctor'
-                  name='name'
-                  className='form-control'
-                  value={doctor.name}
-                  onChange={onChange}
-                />
-              </div>
-              <br />
+              <div className="forms2">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Name of the Doctor"
+                    name="name"
+                    className="form-control"
+                    value={doctor.name}
+                    onChange={onChange}
+                  />
+                </div>
+                <br />
 
-              <div className='form-group'>
-                <input
-                  type='number'
-                  placeholder='medical_expertise'
-                  name='medical_expertise'
-                  className='form-control'
-                  value={doctor.medical_expertise}
-                  onChange={onChange}
-                />
-              </div>
-              <div className='form-group'>
-                <input
-                  type='number'
-                  placeholder='racial_inclusivity'
-                  name='racial_inclusivity'
-                  className='form-control'
-                  value={doctor.racial_inclusivity}
-                  onChange={onChange}
-                />
-              </div>
-
-              <div className='form-group'>
-                <input
-                  type='number'
-                  placeholder='timeliness'
-                  name='timeliness'
-                  className='form-control'
-                  value={doctor.timeliness}
-                  onChange={onChange}
-                />
-              </div>
-
-              <div className='form-group'>
-                <input
-                  type='number'
-                  placeholder='Gender Inclusivity'
-                  name='pronouns'
-                  className='form-control'
-                  value={doctor.pronouns}
-                  onChange={onChange}
-                />
-              </div>
-
-              <div className='form-group'>
-                <input
-                  type='number'
-                  placeholder='Patient Consideration'
-                  name='consideration'
-                  className='form-control'
-                  value={doctor.consideration}
-                  onChange={onChange}
-                />
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Specialty of the Doctor"
+                    name="specialty"
+                    className="form-control"
+                    value={doctor.name}
+                    onChange={onChange}
+                  />
+                </div>
+                <br />
               </div>
 
               <input
-                type='submit'
-                className='btn btn-outline-warning btn-block mt-4'
+                type="submit"
+                className="btn btn-outline-warning btn-block mt-4"
               />
             </form>
           </div>
