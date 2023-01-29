0847f886bbed1a62ebd73f7bd1c807e1878f4cd7
Arathi Nair <arathinair@g.ucla.edu>
2023-01-29 04:42:43 -0800

    changed metrics for doctor vs hospital pages

---
+++ mern_a_to_z_client/src/components/DoctorGraph.js
@@ -0,0 +1,101 @@
+//barchart.js
+import React from "react";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale, // y
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale, // y
+  Tooltip,
+  Legend
+);
+export default function DocGraph(m1, m2, m3, m4, m5) {
+  console.log(m1);
+  const data = {
+    labels: [
+      "Patient Consideration",
+      "Medical Expertise",
+      "Allyship",
+      "Accessibility",
+      "Sensitivity",
+    ],
+    datasets: [
+      {
+        axis: "y",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 2,
+        data: [m1.m1, m1.m2, m1.m3, m1.m4, m1.m5],
+      },
+    ],
+  };
+  const options = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        min: 0,
+        max: 5,
+        stepSize: 1,
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        // position: "right",
+      },
+    },
+  };
+
+  return (
+    <div>
+      <div>
+        <br />
+        <h3>
+          <strong> Ratings</strong>
+        </h3>
+        <div>
+          <Bar data={data} options={options} style={{ width: "600px" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ mern_a_to_z_client/src/components/DoctorRadio.js
@@ -0,0 +1,276 @@
+import axios from "axios";
+import React from "react";
+import "../App.css";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function DocRadio(book, setBook) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const onClick = (event) => {
+    event.preventDefault();
+    var avg_rank_prof = book.book.professionalism;
+    var avg_rank_race = book.book.racial_inclusivity;
+    var avg_rank_lgbtq = book.book.pronouns;
+    var avg_rank_clean = book.book.cleanliness;
+    var avg_rank_help = book.book.helpfulness;
+    var length_reviews = book.book.reviews.length;
+
+    if (event.target.name == "professionalism") {
+      avg_rank_prof =
+        (avg_rank_prof * length_reviews + parseInt(event.target.value)) /
+        length_reviews;
+    } else if (event.target.name == "race") {
+      avg_rank_race =
+        (avg_rank_race * length_reviews + parseInt(event.target.value)) /
+        length_reviews;
+    } else if (event.target.name == "lgbtq") {
+      avg_rank_lgbtq =
+        (avg_rank_lgbtq * length_reviews + parseInt(event.target.value)) /
+        length_reviews;
+    } else if (event.target.name == "clean") {
+      avg_rank_clean =
+        (avg_rank_clean * length_reviews + parseInt(event.target.value)) /
+        length_reviews;
+    } else {
+      avg_rank_help =
+        (avg_rank_help * length_reviews + parseInt(event.target.value)) /
+        length_reviews;
+    }
+
+    const data = {
+      name: book.book.name,
+      address: book.book.address,
+      city: book.book.city,
+      state: book.book.state,
+      zipcode: book.book.zipcode,
+      cleanliness: avg_rank_clean,
+      racial_inclusivity: avg_rank_race,
+      helpfulness: avg_rank_help,
+      pronouns: avg_rank_lgbtq,
+      professionalism: avg_rank_prof,
+    };
+
+    axios
+      .put(`http://localhost:8082/api/books/${id}`, data)
+      .then((res) => {
+        navigate(`/show-book/${id}`);
+      })
+      .catch((err) => {
+        console.log("Error in UpdateBookInfo!");
+      });
+  };
+  return (
+    <div className="rating-inputs">
+      <form className="m-auto">
+        <h3>
+          <strong>Rate the following out of five: </strong>
+        </h3>
+
+        <fieldset>
+          <h2>Patient Consideration:</h2>
+          <div className="radios">
+            <label>
+              &nbsp; 1 &nbsp;
+              <input
+                type="radio"
+                name="professionalism"
+                value="1"
+                onChange={onClick}
+                defaultChecked
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              2 &nbsp;
+              <input
+                type="radio"
+                name="professionalism"
+                value="2"
+                onChange={onClick}
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              3 &nbsp;
+              <input
+                type="radio"
+                name="professionalism"
+                value="3"
+                onChange={onClick}
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              4 &nbsp;
+              <input
+                type="radio"
+                name="professionalism"
+                value="4"
+                onChange={onClick}
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              5 &nbsp;
+              <input
+                type="radio"
+                name="professionalism"
+                value="5"
+                onChange={onClick}
+              />
+              &nbsp;&nbsp;
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <br />
+          <h2>Medical Expertise:</h2>
+          <div className="radios">
+            <label>
+              &nbsp; 1 &nbsp;
+              <input
+                type="radio"
+                name="race"
+                value="1"
+                onChange={onClick}
+                defaultChecked
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              2 &nbsp;
+              <input type="radio" name="race" value="2" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              3 &nbsp;
+              <input type="radio" name="race" value="3" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              4 &nbsp;
+              <input type="radio" name="race" value="4" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              5 &nbsp;
+              <input type="radio" name="race" value="5" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <br />
+          <h2>Allyship:</h2>
+          <div className="radios">
+            <label>
+              &nbsp; 1 &nbsp;
+              <input
+                type="radio"
+                name="lgbtq"
+                value="1"
+                onChange={onClick}
+                defaultChecked
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              2 &nbsp;
+              <input type="radio" name="lgbtq" value="2" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              3 &nbsp;
+              <input type="radio" name="lgbtq" value="3" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              4 &nbsp;
+              <input type="radio" name="lgbtq" value="4" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              5 &nbsp;
+              <input type="radio" name="lgbtq" value="5" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <br />
+          <h2>Accessibility:</h2>
+          <div className="radios">
+            <label>
+              &nbsp; 1 &nbsp;
+              <input
+                type="radio"
+                name="clean"
+                value="1"
+                onChange={onClick}
+                defaultChecked
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              2 &nbsp;
+              <input type="radio" name="clean" value="2" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              3 &nbsp;
+              <input type="radio" name="clean" value="3" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              4 &nbsp;
+              <input type="radio" name="clean" value="4" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              5 &nbsp;
+              <input type="radio" name="clean" value="5" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <br />
+          <h2>Sensitivity:</h2>
+          <div className="radios">
+            <label>
+              1 &nbsp;
+              <input
+                type="radio"
+                name="help"
+                value="1"
+                onChange={onClick}
+                defaultChecked
+              />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              2 &nbsp;
+              <input type="radio" name="help" value="2" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              3 &nbsp;
+              <input type="radio" name="help" value="3" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              4 &nbsp;
+              <input type="radio" name="help" value="4" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+            <label>
+              5 &nbsp;
+              <input type="radio" name="help" value="5" onChange={onClick} />
+              &nbsp;&nbsp;
+            </label>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
